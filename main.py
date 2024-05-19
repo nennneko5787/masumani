b8143c5973baaaa29d1d6888487e6398f62a6d41
Nennneko5787 <nennneko5787@gmail.com>
2024-05-19 17:37:53 +0900

    ooo

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import socketio
 
 from paths import index
 
 fastapi = FastAPI()
 fastapi.include_router(index.router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+fastapi.mount("/static", StaticFiles(directory="static"), name="static")
+
+@fastapi.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.ico")
 
 sio = socketio.AsyncServer(async_mode='asgi')
 app = socketio.ASGIApp(sio, fastapi)
